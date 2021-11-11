2cec1567ec9244e5152ffd489f9f6fbcd0d575c6
Rocky Hotas <rockyhotas@post.com>
2021-11-11 13:24:08 +0100

    Correct wrong comment

--- 02/dev_nr.c
+++ 02/dev_nr.c
@@ -32,7 +32,7 @@ static int driver_open(struct inode *device_file, struct file *instance) {
 }
 
 /**
- * @brief This function is called when the device file is opened
+ * @brief This function is called when the device file is closed
  */
 static int driver_close(struct inode *device_file, struct file *instance) {
 	printk("dev_nr - close was called!\n");
